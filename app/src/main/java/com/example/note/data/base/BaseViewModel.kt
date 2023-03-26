f6b8f92ae51039309adbd69011d8bbd581400ade
swaaage <arsenjanybaev@gmail.com>
2023-03-26 15:03:29 +0600

    Add data package files

---
+++ app/src/main/java/com/example/note/data/base/BaseViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.note.data.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.note.domain.utils.ResultStatus
+import com.example.note.presentation.notes.UiState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel : ViewModel() {
+    protected fun <T> Flow<ResultStatus<T>>.collectFlow(state: MutableStateFlow<UiState<T>>) {
+        viewModelScope.launch {
+            this@collectFlow.collect {
+                when (it) {
+                    is ResultStatus.Error -> state.value = UiState.Error(it.error)
+                    is ResultStatus.Loading -> state.value = UiState.Loading()
+                    is ResultStatus.Succes -> if (it.data != null) {
+                        state.value = UiState.Success(it.data)
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+
