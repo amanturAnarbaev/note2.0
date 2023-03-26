0fa3994a40677b3a80de6818f5194e8aaa3f5e9e
swaaage <arsenjanybaev@gmail.com>
2023-03-26 16:33:08 +0600

    changes from di

--- app/src/main/java/com/example/note/MainActivity.kt
+++ app/src/main/java/com/example/note/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.note
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.note20.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val a = 23
     }
 }
