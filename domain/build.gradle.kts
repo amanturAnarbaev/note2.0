dbe0576b6dd7b3c957542ef2953e92252d3feff4
swaaage <arsenjanybaev@gmail.com>
2023-03-26 13:15:19 +0600

    modified domain/build.gradle.kts

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,9 +1,15 @@
 plugins {
-    id("java-library")
-    id("org.jetbrains.kotlin.jvm")
+    id(Plugin.AGP.javaLibrary)
+    id(Plugin.AGP.jetbrainsKotlin)
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_7
     targetCompatibility = JavaVersion.VERSION_1_7
+}
+
+dependencies {
+    implementation (Dependencies.UI)
+
+    implementation (Dependencies.Coroutine.coroutines)
 }
